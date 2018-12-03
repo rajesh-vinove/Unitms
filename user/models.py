00668b24d07bcbd3eab22917498d2cb40a5eac66
rajesh.sharma@mail.vinove.com <rajesh.sharma@mail.vinove.com>
2018-12-03 19:18:28 +0530

    comment add in model

--- user/models.py
+++ user/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser,Permission, UserManager
 
 
 class User(AbstractUser):
+    """
+    These three field are decide user permission
+    """
     is_driver_user = models.BooleanField(default=False)
     is_normal_user = models.BooleanField(default=False)
     is_admin_user = models.BooleanField(default=False)
